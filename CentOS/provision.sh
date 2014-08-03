d9c002275d1f0ce8026b295ccbacc7bf6c4a07e7
pepepe <sh.pepepe@gmail.com>
2014-08-04 02:07:44 +0900

    chefのインスコ方法を変えた

--- CentOS/provision.sh
+++ CentOS/provision.sh
@@ -7,7 +7,6 @@ service docker start
 chkconfig docker on
 usermod -G docker vagrant
 
-
 # ホストから直接dockerコンテナにアクセスできるようにする
 sed -ir 's/net\.ipv4\.ip_forward = 0/net.ipv4.ip_forward = 1/g' /etc/sysctl.conf
 
@@ -25,36 +24,12 @@ iptables --append FORWARD --in-interface eth0 -j ACCEPT
 service iptables save
 service iptables start
 
-
 # chefをインスコする
-yum -y install gcc zlib-devel openssl-devel sqlite sqlite-devel
-
-# chefりたいので新しいrubyを入れる
-git clone git://github.com/sstephenson/rbenv.git /home/vagrant/.rbenv
-
-echo 'export RBENV_ROOT="/home/vagrant/.rbenv"' >> /home/vagrant/.rbenvrc
-echo 'export PATH="${RBENV_ROOT}/bin:${PATH}"' >> /home/vagrant/.rbenvrc
-echo 'eval "$(rbenv init -)"' >> /home/vagrant/.rbenvrc
-echo "source /home/vagrant/.rbenvrc" >> /home/vagrant/.bash_profile
-
-source /home/vagrant/.rbenvrc
-
-mkdir ${RBENV_ROOT}/plugins
-git clone https://github.com/sstephenson/ruby-build.git ${RBENV_ROOT}/plugins/ruby-build
-
-rbenv install 2.1.2
-rbenv rehash
-rbenv global 2.1.2
-
-chown -R vagrant:vagrant /home/vagrant/.rbenvrc
-chown -R vagrant:vagrant /home/vagrant/.rbenv
-gem install knife-solo --no-ri --no-rdoc
-
+curl -L https://www.opscode.com/chef/install.sh | sudo bash
 
 # dockerコンテナにSSHするための準備
 cp /vagrant/share/id_rsa.docker /home/vagrant/.ssh/id_rsa
 cp /vagrant/share/id_rsa.docker.pub /home/vagrant/.ssh/id_rsa.pub
 chown -R vagrant:vagrant /home/vagrant/.ssh
 chmod -R g-rwx,o-rwx /home/vagrant/.ssh
-
 
